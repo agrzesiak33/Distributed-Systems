5e389edce4462e8d9b75bc64b1a1fe47531ddbde
Alex <agrzesiak@hotmail.com>
2018-04-15 15:17:30 -0700

    70% there

--- Application Server Skeleton/src/appserver/satellite/Satellite.java
+++ Application Server Skeleton/src/appserver/satellite/Satellite.java
@@ -7,6 +7,8 @@ import appserver.comm.Message;
 import static appserver.comm.MessageTypes.JOB_REQUEST;
 import static appserver.comm.MessageTypes.REGISTER_SATELLITE;
 import appserver.job.Tool;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -15,6 +17,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Hashtable;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import utils.PropertyHandler;
@@ -33,27 +36,88 @@ public class Satellite extends Thread {
     private HTTPClassLoader classLoader = null;
     private Hashtable toolsCache = null;
 
-    public Satellite(String satellitePropertiesFile, String classLoaderPropertiesFile, String serverPropertiesFile) {
+    public Satellite(String satellitePropertiesFile, String classLoaderPropertiesFile, String serverPropertiesFile) throws FileNotFoundException {
 
         // read this satellite's properties and populate satelliteInfo object,
         // which later on will be sent to the server
-        // ...
-        
+        File file = new File(satellitePropertiesFile);
+        Scanner inputFile = new Scanner(file);
+
+        while (inputFile.hasNext())
+        {
+            String line = inputFile.nextLine();
+            if(!line.startsWith("#") && line.length() > 0)
+            {
+                String[] parts  = line.split("\t");
+                switch (parts[0].toLowerCase())
+                {
+                    case "host":
+                        
+                        satelliteInfo.setName(parts[1]);
+                        break;
+                    case "port":
+                        satelliteInfo.setPort(Integer.parseInt(parts[1]));
+                        break;
+                }
+            }   
+        }
+        inputFile.close();
         
         // read properties of the application server and populate serverInfo object
         // other than satellites, the as doesn't have a human-readable name, so leave it out
-        // ...
-        
+        file = new File(serverPropertiesFile);
+        inputFile = new Scanner(file);
+
+        while (inputFile.hasNext())
+        {
+            String line = inputFile.nextLine();
+            if(!line.startsWith("#"))
+            {
+                line = line.replace(" ", "");
+                String[] parts  = line.split("=");
+                switch (parts[0].toLowerCase())
+                {
+                    case "host":
+                        serverInfo.setName(parts[1]);
+                        break;
+                    case "port":
+                        serverInfo.setPort(Integer.parseInt(parts[1]));
+                        break;
+                }
+            }   
+        }
+        inputFile.close();
         
         // read properties of the code server and create class loader
-        // -------------------
-        // ...
+        classLoader = new HTTPClassLoader();
+        file = new File(classLoaderPropertiesFile);
+        inputFile = new Scanner(file);
 
-        
+        while (inputFile.hasNext())
+        {
+            String line = inputFile.nextLine();
+            if(!line.startsWith("#"))
+            {
+                line = line.replace(" ", "");
+                String[] parts  = line.split("=");
+                switch (parts[0].toLowerCase())
+                {
+                    case "host":
+                        classLoader.host = parts[1];
+                        break;
+                    case "port":
+                        classLoader.port = Integer.parseInt(parts[1]);
+                        break;
+                    case "doc_root":
+                        classLoader.classRootDir = parts[1];
+                        break;
+                }
+            }   
+        }
+        inputFile.close();
+
         // create tools cache
-        // -------------------
-        // ...
-        
+        toolsCache = new Hashtable();
     }
 
     @Override
@@ -61,17 +125,29 @@ public class Satellite extends Thread {
 
         // register this satellite with the SatelliteManager on the server
         // ---------------------------------------------------------------
-        // ...
-        
+        // TODO
         
         // create server socket
         // ---------------------------------------------------------------
-        // ...
-        
+        ServerSocket incoming = null;
+        try{
+             incoming = new ServerSocket(this.satelliteInfo.getPort());
+        }catch(Exception e){
+            System.err.println("Could not set up server socket");
+        }
         
         // start taking job requests in a server loop
         // ---------------------------------------------------------------
-        // ...
+        try{
+            Socket socket;
+           while(true)
+            {
+                socket = incoming.accept();
+                new Thread(new SatelliteThread(socket, this)).start();
+            } 
+        }catch(Exception e){
+            e.printStackTrace();
+        }        
     }
 
     // inner helper class that is instanciated in above server loop and processes single job requests
@@ -89,21 +165,41 @@ public class Satellite extends Thread {
         }
 
         @Override
-        public void run() {
+        public void run() 
+        {
             // setting up object streams
             // ...
+            try {
+                this.writeToNet = new ObjectOutputStream(this.jobRequest.getOutputStream());
+                this.readFromNet = new ObjectInputStream(this.jobRequest.getInputStream());
+            } catch (IOException ex) {
+                Logger.getLogger(Satellite.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
             
             // reading message
             // ...
-            
-            switch (message.getType()) {
-                case JOB_REQUEST:
-                    // processing job request
-                    // ...
-                    break;
+            boolean running = true;
+            // TODO This loop may not be needed...idk
+            while(running)
+            {
+                try {
+                    this.message = (Message)this.readFromNet.readObject();
+                    switch (message.getType()) 
+                    {
+                        case JOB_REQUEST:
+                            // processing job request
+                            // TODO
+                            break;
 
-                default:
-                    System.err.println("[SatelliteThread.run] Warning: Message type not implemented");
+                        default:
+                            System.err.println("[SatelliteThread.run] Warning: Message type not implemented");
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(Satellite.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(Satellite.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
     }
@@ -122,9 +218,10 @@ public class Satellite extends Thread {
         return toolObject;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // start the satellite
-        Satellite satellite = new Satellite(args[0], args[1], args[2]);
+        Satellite satellite;
+        satellite = new Satellite(args[0], args[1], args[2]);
         satellite.run();
     }
 }
